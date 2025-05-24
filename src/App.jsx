82ed9fc49b28539796e6e85c44409cc9f2402dba
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-05-24 12:56:01 -0500

    feat(data/db): Add simulated database file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
- import Header from "./components/Header"
+import Header from "./components/Header";
+
 function App() {
-
-
   return (
     <>
-      <Header/>
+      <Header />
+
+      <main className="container">
+        <h2 className="text-center">Our Collection</h2>
+
+        <div className="flex-row mt-large"></div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/db.js
@@ -0,0 +1,109 @@
+export const db = [
+  {
+    id: 1,
+    name: "Classic White T-Shirt",
+    image:
+      "https://images.unsplash.com/photo-1644860588182-0998b4ef5587?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "TSH1001",
+    sizes: { S: true, M: true, L: true, XL: false },
+    price: 19.99,
+  },
+  {
+    id: 2,
+    name: "Blue Denim Jacket",
+    image:
+      "https://images.unsplash.com/photo-1601036572102-f8cd483c9518?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "JCK2002",
+    sizes: { S: false, M: true, L: true, XL: true },
+    price: 59.99,
+  },
+  {
+    id: 3,
+    name: "Black Skinny Jeans",
+    image:
+      "https://images.unsplash.com/photo-1588343508462-ddad39b0b0b9?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "JNS3003",
+    sizes: { S: false, M: true, L: true, XL: false },
+    price: 39.99,
+  },
+  {
+    id: 4,
+    name: "Red Hoodie",
+    image:
+      "https://images.unsplash.com/photo-1721111260570-456f3306f8d4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "HD4004",
+    sizes: { S: true, M: true, L: false, XL: false },
+    price: 29.99,
+  },
+  {
+    id: 5,
+    name: "Green Flowy Pants",
+    image:
+      "https://images.unsplash.com/photo-1687825501820-325dbec7550a?q=80&w=1462&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "PNT5005",
+    sizes: { S: false, M: true, L: true, XL: true },
+    price: 45.0,
+  },
+  {
+    id: 6,
+    name: "T-Shirt Yellow",
+    image:
+      "https://images.unsplash.com/photo-1508106611376-a03af6c7d979?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "POL6006",
+    sizes: { S: true, M: true, L: true, XL: true },
+    price: 25.5,
+  },
+  {
+    id: 7,
+    name: "Grey jumpsuit",
+    image:
+      "https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?q=80&w=1380&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "SWP7007",
+    sizes: { S: true, M: true, L: true, XL: true },
+    price: 34.99,
+  },
+  {
+    id: 8,
+    name: "Leather Jacket",
+    image:
+      "https://images.unsplash.com/photo-1602370463198-086436840055?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "JCK8008",
+    sizes: { S: false, M: true, L: true, XL: false },
+    price: 89.99,
+  },
+  {
+    id: 9,
+    name: "Floral Summer Dress",
+    image:
+      "https://images.unsplash.com/photo-1669491477000-6e4ccc35a4ca?q=80&w=1406&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "DRS9009",
+    sizes: { S: true, M: true, L: false, XL: false },
+    price: 49.99,
+  },
+  {
+    id: 10,
+    name: "https://images.unsplash.com/photo-1613594526783-3284e5eb86de?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "",
+    reference: "SHT1010",
+    sizes: { S: true, M: true, L: true, XL: true },
+    price: 28.75,
+  },
+  {
+    id: 11,
+    name: "Black Blazer",
+    image:
+      "https://images.unsplash.com/photo-1701450707728-dc66fd42da83?q=80&w=1484&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "BLZ1111",
+    sizes: { S: false, M: true, L: true, XL: true },
+    price: 69.99,
+  },
+  {
+    id: 12,
+    name: "White Sneakers",
+    image:
+      "https://images.unsplash.com/photo-1663151860150-98ba9a22d696?q=80&w=1454&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    reference: "SNK1212",
+    sizes: { S: false, M: true, L: true, XL: false },
+    price: 59.5,
+  },
+];
