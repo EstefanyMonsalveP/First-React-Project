8fb29c3a53875f5b668ff74a6470f2913643ef6e
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-05-25 15:03:59 -0500

    refactor: Destructure garment object to extract id, image, reference, price, and sizes

--- src/components/Garment.jsx
+++ src/components/Garment.jsx
@@ -1,16 +1,23 @@
 export default function Garment({ garment }) {
   //Select available sizes
-  const availableSizes = Object.entries(garment.sizes)
+
+  const { id, name, image, reference, sizes, price } = garment;
+
+  const availableSizes = Object.entries(sizes)
     .filter(([, available]) => available)
     .map(([size]) => size);
+
+  const handleClick = (id) => {
+    console.log("Diste click", id);
+  };
   return (
     <div className="flex-container column column-md column-lg margin-y-lg">
       <div className="column-lg">
-        <img className="img" src={garment.image} alt="Garment" />
+        <img className="img" src={image} alt="Garment" />
       </div>
       <div>
-        <h2>{garment.name}</h2>
-        <p className="reference">{garment.reference}</p>
+        <h2>{name}</h2>
+        <p className="reference">{reference}</p>
         {/*Only return available sizes*/}
         <div className="column-sizes">
           <div style={{ display: "flex", gap: "8px" }}>
@@ -25,8 +32,10 @@ export default function Garment({ garment }) {
             )}
           </div>
         </div>
-        <p className="price">{garment.price}</p>
-        <button className="btn">Add to cart</button>
+        <p className="price">{price}</p>
+        <button className="btn" onClick={() => handleClick(id)}>
+          Add to cart
+        </button>
       </div>
     </div>
   );
